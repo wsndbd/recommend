4187db35cffb43f3c8c1bc5cf819599d57836727
paldinzhang <paldinzhang@tencent.com>
2017-10-25 20:57:04 +0800

    上传一些代码

--- cainixihuan.py
+++ cainixihuan.py
@@ -1,5 +1,5 @@
-__author__ = 'paldinzhang'
 # -*- coding:utf-8 -*-
+__author__ = 'paldinzhang'
 import csv
 import sys
 import logging
@@ -27,8 +27,23 @@ def silentRemove(filename):
 
 if __name__ == "__main__":
     reader = csv.reader(file("../train.csv", "r"))
-    rowCount = sum(1 for row in reader)
-    print rowCount
-    print len(open("../train.csv").readlines())
-#    for line in reader:
-#        print line
+#    rowCount = sum(1 for row in reader)
+#    print rowCount
+#    print len(open("../train.csv").readlines())
+    itemUsers = {}
+    #uid,iid,score,time
+    #first build inverse table for item to user
+    for i, line in enumerate(reader):
+        if 0 == i:
+            continue
+        if line[1] not in itemUsers:
+            itemUsers[line[1]] = []
+        itemUsers[line[1]].append(line[0])
+        if i > 100:
+            break
+    print itemUsers
+    #计算n(u)和相关度矩阵
+    N = {}
+    C = {}
+    for item, users in itemUsers:
+       print item, users 
