0b65ac501968c1cb01928f706d327aa630dce75f
wsndbd <wsndbd@163.com>
2017-11-04 22:40:18 +0800

    又修改了一些

--- cainixihuan.py
+++ cainixihuan.py
@@ -5,6 +5,7 @@ import logging
 import math
 import operator
 import sqlite3
+import collections
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -60,8 +61,6 @@ if __name__ == "__main__":
         #建立用户到商品的映射
         #print uid, iid, score, timeStamp
         cursor.execute("insert into user_item values(?, ?, ?, ?)", (uid, iid, score, timeStamp))
-        #cursor.execute("select * from user_item")
-        #print cursor.fetchall()
         if i > 1000:
             break
 
@@ -72,33 +71,15 @@ if __name__ == "__main__":
     cursor.execute('''create view if not exists user_score as select
             uid, avg(score) as avg_score, count(iid) as count_iid from user_item group by uid order by uid 
             ''')
-    #cursor.execute("select avg_score from user_score")
-    #print cursor.fetchall()
-    #cursor.execute("select * from sqlite_master where tbl_name = 'user_score' and type = 'view'")
-    #print cursor.fetchall()
     #id1,id2相关度，即打过分的物品共有相同的多少个
     cursor.execute("drop view if exists user_relativity")
     cursor.execute("create view if not exists user_relativity as select u1.uid as uid1, u2.uid as uid2, count(u1.iid) as count_iid from user_item u1 inner join user_item u2 on u1.iid = u2.iid  where u1.uid <> u2.uid group by u1.uid, u2.uid")
-    #cursor.execute("select uid1 from user_relativity")
-    #print cursor.fetchall()
-    #计算n(u)和相关度矩阵*
-    #for u, r in C.iteritems():
-    #    for v, cuv in r.iteritems():
-    #        if u not in W:
-    #            W[u] = {}
-    #        if v not in W[u]:
-    #            W[u][v] = 0
-    #        W[u][v] = cuv / math.sqrt(N[u] * N[v])
-    #print W
-    #for v, wuv in sorted(W['0'].iteritems(), key = operator.itemgetter(1), reverse = True):
-    #    print v, wuv
     #按测试数据计算分数 格式 uid,iid
-    rank = {}
+    rank = collections.OrderedDict()
     K = 80
     reader = csv.reader(file("../test.csv", "r"))
+    next(reader)
     for i, line in enumerate(reader):
-        if 0 == i:
-            continue
         k = 0
         iid = line[1]
         uid = line[0]
@@ -112,26 +93,31 @@ if __name__ == "__main__":
         print "uid", uid
         cursor.execute("select uid2,count_iid from user_relativity where uid1  = ? order by count_iid desc", (uid,))
         W = cursor.fetchall()
-        cursor.execute("select count_iid from user_score where uid = ?", (uid,))
-        ni = cursor.fetchall()[0][0]
+        cursor.execute("select avg_score, count_iid from user_score where uid = ?", (uid,))
+        print "i", i
+        print cursor.fetchall()[0]
+        ni = cursor.fetchall()
+        print "ni", ni
+        selfAvgScore = cursor.fetchall()[0][0]
         for j, item in enumerate(W):
             uid2 = item[0]         
             print "uid2", uid2
-            cursor.execute("select count_iid from user_score where uid = ?", (uid2,))
-            nj = cursor.fetchall()[0][0]
+            cursor.execute("select avg_score, count_iid from user_score where uid = ?", (uid2,))
+            nj = cursor.fetchall()[0][1]
+            avgScore = cursor.fetchall()[0][0]
             print "nj", nj
             wuv = item[1] / math.sqrt(ni * nj)
-            cursor.execute("select score")
-            scoreSum += wuv * 
             if k < K:
-                scoreSum += wuv * float(float(userItems[v][iid][0]) - userAvgScore[v])
+                cursor.execute("select score from user_item where uid = ?", uid2)
+                score = cursor.fetchall()[0][0]
+                scoreSum += wuv * float(float(score) - avgScore)
                 similaritySum += wuv
                 k += 1
                 print "uid", uid, "vid", v, "iid", iid, "score", userItems[v][iid][0], "rank", rank[uid][iid], "k", k
         if 0 == k:
             rank[uid][iid] = 0.0
         else:
-            rank[uid][iid] = userAvgScore[uid] + scoreSum / similaritySum
+            rank[uid][iid] = selfAvgScore + scoreSum / similaritySum
         print "uid", uid, "avgScore", userAvgScore[uid], "iid", iid, "rank", rank[uid][iid], "k", k, "scoreSum", scoreSum, "similaritySum", similaritySum
     #print rank
     testFile = open("test.csv", "w")
