f2e84113fcbabfc6d57b466773d7f0a09cf628af
paldinzhang <paldinzhang@tencent.com>
2017-11-05 19:59:09 +0800

    数据库的基本整理完了，但是效率太低

--- cainixihuan.py
+++ cainixihuan.py
@@ -30,7 +30,7 @@ def silentRemove(filename):
 
 def tableExist(cursor, tableName):
     cursor.execute("select name from sqlite_master where type = 'table' and name = '?'", (tableName))
-    if 0 == len(cursor.fetchall()):
+    if None == cursor.fetchone():
         return False
     else:
         return True
@@ -60,20 +60,21 @@ if __name__ == "__main__":
         timeStamp = line[3]
         #建立用户到商品的映射
         #print uid, iid, score, timeStamp
+        print "(uid, iid, score, timeStamp)", (uid, iid, score, timeStamp)
         cursor.execute("insert into user_item values(?, ?, ?, ?)", (uid, iid, score, timeStamp))
-        if i > 1000:
-            break
 
     #create index
     cursor.execute("drop index if exists index_on_user_item")
     cursor.execute("create index if not exists index_on_user_item on user_item(uid, iid)")
-    cursor.execute("drop view if exists user_score")
-    cursor.execute('''create view if not exists user_score as select
+    cursor.execute("drop table if exists user_score")
+    cursor.execute('''create table if not exists user_score as select
             uid, avg(score) as avg_score, count(iid) as count_iid from user_item group by uid order by uid 
             ''')
     #id1,id2相关度，即打过分的物品共有相同的多少个
-    cursor.execute("drop view if exists user_relativity")
-    cursor.execute("create view if not exists user_relativity as select u1.uid as uid1, u2.uid as uid2, count(u1.iid) as count_iid from user_item u1 inner join user_item u2 on u1.iid = u2.iid  where u1.uid <> u2.uid group by u1.uid, u2.uid")
+    cursor.execute("ANALYZE")
+    cursor.execute("drop table if exists user_relativity")
+    cursor.execute("create table if not exists user_relativity as select u1.uid as uid1, u2.uid as uid2, count(u1.iid) as count_iid from user_item u1 inner join user_item u2 on u1.iid = u2.iid  where u1.uid > u2.uid group by u1.uid, u2.uid")
+    quit()
     #按测试数据计算分数 格式 uid,iid
     rank = collections.OrderedDict()
     K = 80
@@ -90,35 +91,48 @@ if __name__ == "__main__":
         scoreSum = 0
         similaritySum = 0
         #计算相似度
-        print "uid", uid
-        cursor.execute("select uid2,count_iid from user_relativity where uid1  = ? order by count_iid desc", (uid,))
-        W = cursor.fetchall()
+        #找到自身的平均分及评价过的商品个数
         cursor.execute("select avg_score, count_iid from user_score where uid = ?", (uid,))
-        print "i", i
-        print cursor.fetchall()[0]
-        ni = cursor.fetchall()
-        print "ni", ni
-        selfAvgScore = cursor.fetchall()[0][0]
-        for j, item in enumerate(W):
-            uid2 = item[0]         
-            print "uid2", uid2
-            cursor.execute("select avg_score, count_iid from user_score where uid = ?", (uid2,))
-            nj = cursor.fetchall()[0][1]
-            avgScore = cursor.fetchall()[0][0]
-            print "nj", nj
-            wuv = item[1] / math.sqrt(ni * nj)
+        row = cursor.fetchone()
+        if not row:
+            continue
+        selfAvgScore = row[0]
+        ni = row[1]
+        print "i", i, "uid", uid, "selfAvgScore", selfAvgScore, "ni", ni 
+        #找到和uid相关的所有用户,按相关度降序排序
+        cursor.execute("select uid2,count_iid from user_relativity where uid1  = ? order by count_iid desc", (uid,))
+        #计算每个用户对iid的打分
+        for row in cursor:
+            uid2 = row[0]
+            sameCount = row[1]
+            print "uid2", uid2, "sameCount", sameCount
+            #找到相关用户对应的平均分及评价物品的个数
+            cur1 = conn.cursor()
+            cur1.execute("select avg_score, count_iid from user_score where uid = ?", (uid2,))
+            row1 = cur1.fetchone()
+            avgScore = row1[0]
+            nj = row1[1]
+            print "uid2", uid2, "avgScore", avgScore, "nj", nj
+            #相似度
+            wuv = sameCount / math.sqrt(ni * nj)
             if k < K:
-                cursor.execute("select score from user_item where uid = ?", uid2)
-                score = cursor.fetchall()[0][0]
+                print "uid2", uid2, "iid", iid
+                cur1.execute("select score from user_item where uid = ? and iid = ?", (uid2, iid))
+                row1 = cur1.fetchone()
+                if not row1:
+                    continue
+                #相关用户对iid的打分
+                score = row1[0]
+                print "uid2", uid2, "iid", iid, "score", score
                 scoreSum += wuv * float(float(score) - avgScore)
                 similaritySum += wuv
                 k += 1
-                print "uid", uid, "vid", v, "iid", iid, "score", userItems[v][iid][0], "rank", rank[uid][iid], "k", k
         if 0 == k:
             rank[uid][iid] = 0.0
         else:
             rank[uid][iid] = selfAvgScore + scoreSum / similaritySum
-        print "uid", uid, "avgScore", userAvgScore[uid], "iid", iid, "rank", rank[uid][iid], "k", k, "scoreSum", scoreSum, "similaritySum", similaritySum
+        if i > 2:
+            break
     #print rank
     testFile = open("test.csv", "w")
     writer = csv.writer(testFile)
