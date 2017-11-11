7471c9057c05dde4b4fe965fed98a152e475a754
paldinzhang <paldinzhang@tencent.com>
2017-11-11 18:46:32 +0800

    完成了用sparse的方式，在计算相似度的时候耗时最长，还是不太可用

--- cainixihuan.py
+++ cainixihuan.py
@@ -39,37 +39,46 @@ if __name__ == "__main__":
     reCreate = False
     opts, args = getopt.getopt(sys.argv[1:], "c", [])
     for o, a in opts:
-        print o, a
         #recreate database
         if "-c" == o:
             reCreate = True 
             silentRemove("./dict.db")
-    conn = sqlite3.connect("./dict.db")
-    cursor = conn.cursor()
     #table not exist
     if reCreate:
-        data = pd.read_csv("../train2.csv")
+        print "pandas reading..."
+        startTime = time.time()
+        data = pd.read_csv("../train.csv")
+        print "eclapsed time", time.time() - startTime
         #uid,iid,score,time
         uids = set()
         iids = set()
         maxUid = 0
         maxIid = 0
+
+        print "reading file..."
+        startTime = time.time()
         for row in data.values:
-            print "row0", row[0], "row1", row[1]
+            #print ".",
             uids.add(row[0])
             iids.add(row[1])
             if row[0] > maxUid:
                 maxUid = row[0]
             if row[1] > maxIid:
                 maxIid = row[1]
+        print "eclapsed time", time.time() - startTime
 
         print "len(uids)", len(uids), "len(iids)", len(iids)
         print "maxUid", maxUid, "maxIid", maxIid
+        print "creating matrix..."
+        startTime = time.time()
         m = sparse.lil_matrix((maxUid + 1, maxIid + 1), dtype = np.int)
         for row in data.values:
+            #print "row0", row[0], "row1", row[1], "row2", row[2]
             m[row[0], row[1]] = row[2]
+        print "eclapsed time", time.time() - startTime
 
     #id1,id2相关度，即打过分的物品共有相同的多少个
+    rowCount = sum(1 for row in file("../test.csv", "r"))
     rank = collections.OrderedDict()
     K = 80
     reader = csv.reader(file("../test.csv", "r"))
@@ -77,12 +86,10 @@ if __name__ == "__main__":
     lastUid = 0
     W = {}
     for i, line in enumerate(reader):
-        if i > 3:
-            break
         k = 0
         iid = int(line[1])
         uid = int(line[0])
-        if line[0] not in rank:
+        if uid not in rank:
             rank[uid] = {}
         if iid not in rank[uid]:
             rank[uid][iid] = 0
@@ -90,35 +97,51 @@ if __name__ == "__main__":
         similaritySum = 0
         #计算相似度
         #找到自身的平均分及评价过的商品个数
+        if uid >= m.shape[0]:
+            print "error row index", uid, " out of bounds", m.shape[0]
+            continue
         arrayU = m[uid,:].toarray()
         ni = np.count_nonzero(arrayU)
         selfAvgScore = float(arrayU.sum()) / ni  
-        print "i", i, "uid", uid, "selfAvgScore", selfAvgScore, "ni", ni 
+        print "calculate similarrity", i, "of", rowCount
+        startTime = time.time()
+        #print "i", i, "uid", uid, "selfAvgScore", selfAvgScore, "ni", ni,
         #找到和uid相关的所有用户,按相关度降序排序
-        if uid != lastUid:
+        if uid != lastUid or 0 == lastUid:
             W = {}
+            #print "caclatuing same count"
             for vid in xrange(maxUid + 1):
                 if uid != vid:
+                    #print "uid", uid, "vid", vid, "sameCount",
                     arrayV = m[vid,:].toarray()
                     #先用sign函数把打过的分转换成1，0，-1，再相与出都是1的个数，就是共同参与过打分的物品数
                     W[vid] = np.count_nonzero(np.logical_and(np.sign(arrayU), np.sign(arrayV)))
+                    #print W[vid]
         #计算每个用户对iid的打分
+        print "eclapsed time", time.time() - startTime
+        print "calculate score", i, "of", rowCount
+        startTime = time.time()
+        #print "calclating score"
         for u2, w in sorted(W.iteritems(), key = operator.itemgetter(1), reverse = True): 
             uid2 = u2
             sameCount = w
+            #print "uid", uid, "uid2", uid2, "sameCount", sameCount,
             #找到相关用户对应的平均分及评价物品的个数
             arrayV = m[uid2,:].toarray()
             nj = np.count_nonzero(arrayV)
+            if 0 == nj:
+                #print
+                continue
             avgScore = float(arrayV.sum()) / nj
             #相似度
             wuv = float(sameCount) / math.sqrt(ni * nj)
-            print "uid", uid, "uid2", uid2, "avgScore", avgScore, "nj", nj, "sameCount", sameCount, "wuv", wuv
+            #print "nj", nj, "avgScore", avgScore, "wuv", wuv
             if k < K:
                 #相关用户对iid的打分
                 score = arrayV[0][iid]
                 if 0 == score:
                     continue
-                print "k", k, "uid2", uid2, "iid", iid, "score", score
+                #print "k", k, "uid2", uid2, "iid", iid, "score", score
                 scoreSum += wuv * float(float(score) - avgScore)
                 similaritySum += wuv
                 k += 1
@@ -128,9 +151,12 @@ if __name__ == "__main__":
             rank[uid][iid] = 0.0
         else:
             rank[uid][iid] = selfAvgScore + scoreSum / similaritySum
+        print "eclapsed time", time.time() -startTime
         lastUid = uid
 
     #print rank
+    print "write score to file..."
+    startTime = time.time()
     testFile = open("test.csv", "w")
     writer = csv.writer(testFile)
     writer.writerow(['uid', 'iid', 'score'])
@@ -138,3 +164,4 @@ if __name__ == "__main__":
         for v, s in rank[u].iteritems():
             writer.writerow([u, v, s])
     testFile.close() 
+    print "eclapsed time", time.time() -startTime
